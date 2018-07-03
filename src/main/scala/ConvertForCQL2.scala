fe9b1bb460672d4fabe44c754f81d4d329cbc8bd
TomLous <tomlous@gmail.com>
2018-07-03 16:10:13 +0200

    C* insert statements

--- src/main/scala/ConvertForCQL2.scala
+++ src/main/scala/ConvertForCQL2.scala
@@ -2,18 +2,18 @@ import java.io._
 import java.text.SimpleDateFormat
 import java.util.TimeZone
 
-object ConvertForCQL extends App {
+object ConvertForCQL2 extends App {
 
   val tenant = "ebay_kleinanzeigen"
   val sdf = new SimpleDateFormat("yyyy-MM-dd")
   sdf.setTimeZone(TimeZone.getTimeZone("UTC"))
-  val fileTimeStampMicroSeconds = sdf.parse("2018-05-04").toInstant.toEpochMilli * 1000
+  val fileTimeStampMicroSeconds = sdf.parse("2018-06-20").toInstant.toEpochMilli * 1000
 
-  val inputFile = new File("/Users/tlous/Downloads/import/2018-05-04_LM_User_Last_Activity_Date.txt")
+  val inputFile = new File("/Users/tlous/Downloads/import/part-00000-b438acac-1d6a-42de-b465-8d59ea55146e-c000.csv")
   //  val inputFile = new File("/Users/tlous/Downloads/import/tmp.txt")
-  val outputFile = new File(s"/Users/tlous/Downloads/import/$tenant.cql")
+  val outputFile = new File(s"/Users/tlous/Downloads/import/$tenant-2.cql")
 
-  val fileSDF = new SimpleDateFormat("M/d/yyyy")
+  val fileSDF = new SimpleDateFormat("yyyy-MM-dd")
   fileSDF.setTimeZone(TimeZone.getTimeZone("UTC"))
 
   val writer = new BufferedOutputStream(new FileOutputStream(outputFile, false))
@@ -22,12 +22,13 @@ object ConvertForCQL extends App {
   var counter = 0
 
   try {
+
     writer.write(s"CONSISTENCY LOCAL_QUORUM;\n".getBytes)
     Stream.continually(reader.readLine()).drop(1).takeWhile(_ != null).foreach(
       line => {
-        val parts = line.split("\t").toList
-        val vid = parts(1)
-        val epochMilli = fileSDF.parse(parts(2)).toInstant.toEpochMilli
+        val parts = line.split(",").toList
+        val vid = parts(0)
+        val epochMilli = fileSDF.parse(parts(1)).toInstant.toEpochMilli
 
         val q = s"INSERT INTO $tenant.user (visitor_id, last_active_epoch_millis) VALUES ('u$vid', $epochMilli) USING  TIMESTAMP $fileTimeStampMicroSeconds;"
 
---
+++ src/main/scala/MinHashPerformance.scala
@@ -0,0 +1,124 @@
+import org.apache.spark.ml.Pipeline
+import org.apache.spark.ml.feature.{HashingTF, MinHashLSH, MinHashLSHModel, Tokenizer}
+import org.apache.spark.ml.param.{IntParam, Param}
+import org.apache.spark.ml.util.Identifiable
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+
+object MinHashPerformance  extends App {
+
+  val logger = org.apache.log4j.LogManager.getLogger(getClass.getName.init)
+
+  // config
+  val shingleSize = 2
+  val numFeatures = 100000
+  val numHashTables = 2
+
+  // Pipeline
+  val tokenizer = new ShingleTokenizer()
+    .setShingleSize(shingleSize)
+    .setInputCol("location")
+    .setOutputCol("parts")
+
+  val hashingTF = new HashingTF()
+    .setNumFeatures(numFeatures)
+    .setInputCol(tokenizer.getOutputCol)
+    .setOutputCol("rawFeatures")
+
+  val minHash = new MinHashLSH()
+    .setNumHashTables(numHashTables)
+    .setInputCol(hashingTF.getOutputCol)
+    .setOutputCol("hashes")
+
+  val pipeline = new Pipeline()
+    .setStages(Array(tokenizer, hashingTF, minHash))
+
+  // test tokenizer
+  val testTokenize = tokenizer.asInstanceOf[ShingleTokenizer].createTransformFunc
+  println(testTokenize("Denville|07834|NJ|New Jersey"))
+
+
+
+  // Spark
+  logger.info("Start")
+  val spark = SparkSession.builder().master("local[*]").appName("test").getOrCreate()
+  import spark.implicits._
+
+  logger.info("Load cities")
+  val cities = spark.read.parquet("/tmp/cities_exploded.parquet")
+  logger.info(s"cities Info: # records ${cities.count}, # partitions ${cities.rdd.partitions.length}")
+
+
+  logger.info("Load checkLocations")
+  val checkLocations = spark.read.parquet("/tmp/exploded.parquet")
+  logger.info(s"checkLocations Info: # records ${checkLocations.count}, # partitions ${checkLocations.rdd.partitions.length}")
+
+  // Train the model
+  logger.info("Train LSH model")
+  val model = pipeline.fit(cities)
+
+
+  // Transform the data
+  logger.info("Transform cities")
+  val citiesTransformed = model.transform(cities)
+  logger.info(s"citiesTransformed Info: # records ${citiesTransformed.count}, # partitions ${citiesTransformed.rdd.partitions.length}")
+  citiesTransformed.show(3)
+  citiesTransformed.printSchema()
+
+  logger.info("Transform checks")
+  val checkLocationsTransformed = model.transform(checkLocations)
+  logger.info(s"checkLocationsTransformed Info: # records ${checkLocationsTransformed.count}, # partitions ${checkLocationsTransformed.rdd.partitions.length}")
+  checkLocationsTransformed.show(3)
+  checkLocationsTransformed.printSchema()
+
+  // Get the minhash part
+  val minHashLSHModel = model.stages.last.asInstanceOf[MinHashLSHModel]
+
+  logger.info("approxSimilarityJoin")
+  val joined = minHashLSHModel.approxSimilarityJoin(citiesTransformed, checkLocationsTransformed, 0.6)
+
+  logger.info(s"Joined info: # records ${joined.count}, # partitions ${joined.rdd.partitions.length}")
+  joined.show(3)
+  joined.printSchema()
+
+  logger.info("End")
+
+  /**
+    * Custom Shingle Tokenizer.
+    * Needed shingling to account for typo's & spelling mistakes.
+    * But I didn't want to create non-informative shingles for information parts that are independent:
+    *
+    * eg. "Denville|07834|NJ|New Jersey" will become
+    * ArraySeq( d, de, en, nv, vi, il, ll, le, e ,  0, 07, 78, 83, 34, 4 ,  n, nj, j ,  n, ne, ew, w ,  j, je, er, rs, se, ey, y )
+    *
+    */
+  class ShingleTokenizer(override val uid: String) extends Tokenizer {
+    final val defaultShingleSize = 2
+    final val defaultSplitPartChar = '|'
+    final val shingleSize = new IntParam(this, "shingleSize", "k-shingle size")
+    final val splitPartChar = new Param[Char](this, "splitChar", "char to split the string on")
+
+    def this() = this(Identifiable.randomUID("shingleTokenizer"))
+
+    def setShingleSize(value: Int): ShingleTokenizer = set(shingleSize, value).asInstanceOf[ShingleTokenizer]
+
+    def setSplitChar(value: Char): ShingleTokenizer = set(splitPartChar, value).asInstanceOf[ShingleTokenizer]
+
+    override def createTransformFunc: String => Seq[String] = {
+      _
+        .toLowerCase
+        .split(get[Char](splitPartChar).getOrElse(defaultSplitPartChar))
+        .flatMap(part =>
+          s" $part "
+            .replaceAll("""[^a-z0-9]+""", " ")
+            .toCharArray
+            .sliding(get[Int](shingleSize).getOrElse(defaultShingleSize))
+            .map(_.mkString)
+            .toIndexedSeq
+        )
+    }
+
+    // Used to show off m
+    //    def publicCreateTransformFunc = createTransformFunc
+  }
+}
